6c7c7a45251acf2dbef344ee2858ede5bb7ceb06
manuel iglesias <m.iglesias91@gmail.com>
2017-12-10 23:45:28 -0300

    cambios en los test de twitter.

--- testing/test_twitter.cpp
+++ testing/test_twitter.cpp
@@ -18,16 +18,16 @@ using namespace scraping::twitter;
 
 TEST(ScrapingTwitter, obtenerUltimosTweets)
 {
-    //scraping::twitter::ConsumidorAPI * consumidor_api_twitter = new scraping::twitter::ConsumidorAPI("aXPpXInb16k0jKIswOYuUS3ly", "MhuDBpN9EtnafrIUHvJEbleJ3WKiFCSBIulwRVNvZTWoXGs2eV");
-    //consumidor_api_twitter->obtenerTokenDeAcceso();
+    scraping::twitter::ConsumidorAPI * consumidor_api_twitter = new scraping::twitter::ConsumidorAPI("aXPpXInb16k0jKIswOYuUS3ly", "MhuDBpN9EtnafrIUHvJEbleJ3WKiFCSBIulwRVNvZTWoXGs2eV");
+    consumidor_api_twitter->obtenerTokenDeAcceso();
 
-    //scraping::twitter::modelo::Aplicacion app(consumidor_api_twitter);
+    scraping::twitter::modelo::Aplicacion app(consumidor_api_twitter);
 
-    //scraping::twitter::modelo::Usuario clarin("clarincom");
+    scraping::twitter::modelo::Usuario clarin("clarincom");
 
-    //std::vector<scraping::twitter::modelo::Tweet> tweets = app.leerUltimosTweets(&clarin);
+    std::vector<scraping::twitter::modelo::Tweet> tweets = app.leerUltimosTweets(&clarin);
 
-    //ASSERT_EQ(true, true);
+    ASSERT_EQ(true, true);
 }
 
 TEST(ScrapingTwitter, parsearTweetsCorrectamente)
