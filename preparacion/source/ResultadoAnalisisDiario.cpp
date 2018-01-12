305c1d0fce58d25971262186b221de67b7bf0237
manueli <m.iglesias91@gmail.com>
2018-01-12 13:06:04 -0300

    cambios ResultadoAnalisisDiario.

--- preparacion/source/ResultadoAnalisisDiario.cpp
+++ preparacion/source/ResultadoAnalisisDiario.cpp
@@ -17,6 +17,13 @@ ResultadoAnalisisDiario::~ResultadoAnalisisDiario()
 
 // GETTERS
 
+// getters de IAlmacenable
+
+std::string ResultadoAnalisisDiario::getValorAlmacenable()
+{
+
+}
+
 // SETTERS
 
 // METODOS
@@ -26,7 +33,39 @@ void ResultadoAnalisisDiario::agregarMedio(ResultadoAnalisisMedio * resultado_me
     this->resultados_medios.push_back(resultado_medio);
 }
 
+// metodos de IContieneJson
+
+bool ResultadoAnalisisDiario::armarJson()
+{
+    this->getJson()->reset();
+
+    for (std::vector<ResultadoAnalisisMedio*>::iterator it = this->resultados_medios.begin(); it != this->resultados_medios.end(); it++)
+    {
+        std::string id_medio = (*it)->getId()->string();
+        herramientas::utiles::Json * json = (*it)->getJson();
+
+        this->getJson()->agregarAtributoJson(id_medio, json);
+    }
+
+    return true;
+}
+
+bool ResultadoAnalisisDiario::parsearJson()
+{
+
+}
+
 // metodos de IAlmacenable
+
+void ResultadoAnalisisDiario::asignarNuevoId()
+{
+
+}
+
+void ResultadoAnalisisDiario::parsearValorAlmacenable(std::string valor_almacenable)
+{
+
+}
 
 std::string ResultadoAnalisisDiario::prefijoGrupo()
 {
